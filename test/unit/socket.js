cbe3f23a8120566b04f6c394248cba237a090a55
cyberfunk <hello@cyberfunk.org>
2014-02-08 17:02:58 +0800

    Assert on -S expression.

---
+++ test/unit/socket.js
@@ -0,0 +1,87 @@
+var net = require('net');
+var fs = require('fs');
+var path = require('path');
+var Socket = net.Socket;
+var expect = require('chai').expect;
+var test = require('../..').test;
+var run = require('../util/run');
+var files = require('../util/files');
+
+var server, client, port = 8124, fd, file;
+
+describe('cli-fs:', function() {
+  before(function(done) {
+    server = net.createServer(function(c) {
+      c.on('end', function() {});
+      c.pipe(c);
+    });
+    server.listen(port, function() {
+      client = new Socket();
+      client.connect(port);
+      client.on('connect', function(){
+        fd = server._handle.fd;
+        file = path.join(files.dev, 'fd', '' + fd);
+        done();
+      });
+    });
+  })
+  after(function(done) {
+    client.on('close', function() {
+      server.close(function() {
+        done();
+      })
+    });
+    client.end();
+  })
+  it('should test socket - missing (false)',
+    function(done) {
+      var result = false;
+      var expr = '-S';
+      var value = files.missing;
+      var args = [expr, value];
+      run(args, function(expected) {
+        expect(result).to.eql(expected);
+        var res = test(expr, value);
+        expect(res).to.eql(expected);
+        test(expr, value, function(res) {
+          expect(res).to.eql(expected);
+          done();
+        });
+      });
+    }
+  );
+  it('should test socket - zero (false)',
+    function(done) {
+      var result = false;
+      var expr = '-S';
+      var value = files.zero;
+      var args = [expr, value];
+      run(args, function(expected) {
+        expect(result).to.eql(expected);
+        var res = test(expr, value);
+        expect(res).to.eql(expected);
+        test(expr, value, function(res) {
+          expect(res).to.eql(expected);
+          done();
+        });
+      });
+    }
+  );
+  it('should test socket (true)',
+    function(done) {
+      var result = true;
+      var expr = '-S';
+      var value = file;
+      var args = [expr, fd];
+      // NOTE: test -S /dev/fd/12 does not return true
+      // NOTE: regardless of whether the server or client fd
+      // NOTE: is used
+      var res = test(expr, value);
+      expect(res).to.eql(result);
+      test(expr, value, function(res) {
+        expect(res).to.eql(result);
+        done();
+      });
+    }
+  );
+})
